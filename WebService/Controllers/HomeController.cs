f044da523a922842b6dab5bf99c9929bccb901ed
tboneteen18 <tboneteen18@gmail.com>
2016-07-20 16:46:17 -0600

    adding project to github

---
+++ WebService/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using WebService.Models;
+
+namespace WebService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(WeatherModel model)
+        {
+            return View(model);
+        }
+
+        WeatherService.WeatherSoapClient client = new WeatherService.WeatherSoapClient("WeatherSoap");
+
+        [HttpPost]
+        public ActionResult Weather(WeatherModel model)
+        {
+            var forcast = client.GetCityWeatherByZIP(model.ZipCode);
+            ViewBag.MyList = "the temperature in ";
+            ViewBag.MyList += forcast.City.ToString();
+            ViewBag.MyList += " is ";
+            ViewBag.MyList += forcast.Temperature.ToString();
+            ViewBag.MyList += " Degrees";
+            return View(model);
+        }
+    }
+}
---
+++ WebService/Models/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebService.Models
+{
+    public class WeatherModel
+    {
+        [Required (ErrorMessage = "Please enter your zipcode")]
+        public string ZipCode { get; set; }
+    }
+}
